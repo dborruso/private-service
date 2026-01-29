3ce9f460618ed0a939d0dcfc52a79ed811363b58
Davide Borruso <195457001+dborruso@users.noreply.github.com>
2026-01-29 23:22:12 +0100

    feat: implement API endpoint to fetch instruments from Supabase

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+import os
 from flask import Flask, jsonify
+from supabase import create_client, Client
 
 app = Flask(__name__)
 
+supabase: Client = create_client(
+    os.environ.get("SUPABASE_URL"),
+    os.environ.get("SUPABASE_PUBLISHABLE_KEY")
+)
+
 @app.route('/api/test')
 def test():
-    return jsonify({"data": "test"})
+    response = supabase.table('instruments').select("*").execute()
+    instruments = response.data
+    return jsonify(instruments)
